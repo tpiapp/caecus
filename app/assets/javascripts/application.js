1ffac4c4451d5bb41bb6072c287c561679a2c669
Juan Castelblanco <jscastelblancoh@unal.edu.co>
2019-07-01 13:52:44 -0500

    Vistas de formulario aadidas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function () {
+	
+	$('.datepicker').datepicker({
+		format: 'yyyy-mm-dd',
+		i18n: {
+			months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+			monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+			weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+			weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+		},
+		selectMonths: true,
+		selectYears: 100, // Puedes cambiarlo para mostrar más o menos años
+		today: 'Hoy',
+		clear: 'Limpiar',
+		close: 'Ok',
+		labelMonthNext: 'Siguiente mes',
+		labelMonthPrev: 'Mes anterior',
+		labelMonthSelect: 'Selecciona un mes',
+		labelYearSelect: 'Selecciona un año',
+	}); // Formateo de la visualización de la fecha
+	
+	$('select').formSelect(); //Sin esto los select no se muestran
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/users_panel' =>'user_panel#index', :as => :user_panel
+  get '/confirmation' =>'confirmation#index', :as => :confirmation
 
   
   root "landing_page#index"
